c8fdeb15b46a3b077b425d6f7f038594cb12edba
spemer <ghsspower@naver.com>
2018-08-15 07:15:49 +0900

    ADD meta ogimg

--- src/App.vue
+++ src/App.vue
@@ -18,14 +18,11 @@ export default {
       {property: 'og:description', content: '모바일 가구거리, 뻔뻐'},
       {property: 'og:type', content: 'website'},
       {property: 'og:url', content: 'https://funfur-info.github.io'},
-      {property: 'og:image', content: '/src/assets/dist/metacard.png'},
       {name: 'twitter:card', content: 'summary'},
       {name: 'twitter:title', content: '모바일 가구거리, 뻔뻐'},
       {name: 'twitter:description', content: '모바일 가구거리, 뻔뻐'},
-      {name: 'twitter:image:src', content: '/src/assets/dist/metacard.png'},
       {itemprop: 'name', content: '모바일 가구거리, 뻔뻐'},
       {itemprop: 'description', content: '모바일 가구거리, 뻔뻐'},
-      {itemprop: 'image', content: '/src/assets/dist/metacard.png'}
     ],
     links: [
       {rel: 'canonical', href: 'https://funfur-info.github.io'}
