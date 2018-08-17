bacc0d2114d28d4c1eba4a3f1f25f278d0780ba6
spemer <ghsspower@naver.com>
2018-08-18 05:49:24 +0900

    DELETE App.vue metainfo

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,6 @@
 
 <script>
 export default {
-  name: 'app',
-  metaInfo: {
-    title: '모바일 가구거리, 뻔뻐',
-    titleTemplate: '%s',
-    htmlAttrs: {
-      lang: 'ko'
-    },
-    meta: [
-      {property: 'og:title', content: '모바일 가구거리, 뻔뻐'},
-      {property: 'og:type', content: 'website'},
-      // {property: 'og:url', content: 'https://funfur-info.github.io'},
-      {name: 'twitter:card', content: 'summary'},
-      {name: 'twitter:title', content: '모바일 가구거리, 뻔뻐'},
-    ],
-    // links: [
-    //   {rel: 'canonical', href: 'https://funfur-info.github.io'}
-    // ]
-  }
+  name: 'app'
 }
 </script>
