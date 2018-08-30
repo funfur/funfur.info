2a0342cac4594b647851aeb3537ce28fb5faf06c
spemer <ghsspower@naver.com>
2018-08-30 15:55:06 +0900

    DOC EDIT tos, policy details

--- src/globalVar.js
+++ src/globalVar.js
@@ -5,12 +5,14 @@ export const globalVar = new Vue ({
     companyId: '435-15-00646',
     companyAddress: '서울특별시 성동구 왕십리로 222 한양대학교 HIT관 B234호 (04763)',
     companyName: '뻔뻔한친구들',
+    companyNameShort: '뻔뻐',
     ceoName: '유덕열',
 
     callTo: '050-6591-6200',
     faxTo: '0504-433-6202',
     mailTo: 'official@funfur.kr',
 
+    websiteUrl: 'https://funfur.info/',
     ceoWebsiteUrl: 'https://ceo.funfur.kr/',
     ceoRegisterWebsiteUrl: 'https://ceo.funfur.kr/register',
 
